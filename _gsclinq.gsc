2f46598059c2189335cc79cd9a39ecbf12d5a831
lol <suxlolz1528@gmail.com>
2019-10-31 15:01:23 +0100

    Added: Last First Average Count Range Repeat Reverse, TODO sorting func

--- _gsclinq.gsc
+++ _gsclinq.gsc
@@ -30,14 +30,15 @@ test()
     for (i = 0; i < 10; i++)
         arr[i] = i;
 
+    comprintf("test func\n");
+
     while (true)
     {
-        array = where(arr, arr.size, ::testPredicate);
-        printNewArray(array);
-        // printResultArray(array);
-        
-        // isAll = all(arr, arr.size, ::testPredicate);
-        // printResultArray(isAll);
+        //array = repeat(arr, arr.size, 2);
+        //printNewArray(array);
+
+        isAll = count(arr, arr.size, ::testPredicate);
+        printResultArray(isAll);
         wait 5;
     }
 }
@@ -60,137 +61,6 @@ testPlayer()
     // }
 }
 
-reverse(array)
-{
-    arr = [];
-    index = 0;
-
-    for (i = array.size; i != 0; i--)
-    {
-        arr[index] = array[i];
-        index++;
-    }
-    return arr;
-}
-
-repeat(array, count)
-{
-    arr = [];
-    index = 0;
-
-    for (r = 0; r < count; r++)
-    {
-        for (i = 0; i < array.size; i++)
-        {
-            arr[index] = array[i];
-            index++;
-        }
-    }
-    return arr;
-}
-
-range(array, min, max)
-{
-    arr = [];
-    index = 0;
-    if (!isDefined(max))
-        max = array.size;
-
-    for (i = min; i < max; i++)
-    {
-        arr[index] = array[i];
-        index++;
-    }
-    return arr;
-}
-
-select(array, func)
-{
-    arr = [];
-
-    for (i = 0; i < array.size; i++)
-        array[i] = [[func]](array[i]);
-    return arr;
-}
-
-sum(array)
-{
-    ret = 0;
-    for (i = 0; i < array.size; i++)
-        ret += array[i];
-    return ret;
-}
-
-count(array, predicate)
-{
-    ret = 0;
-    for (i = 0; i < array.size; i++)
-    {
-        if ([[predicate]](array[i]))
-            ret++;
-    }
-    return ret;
-}
-
-average(array)
-{
-    sum = array[0];
-    count = 1;
-    for (i = 0; i < array.size; i++)
-    {
-        sum += array[i];
-        count++;
-    }
-    return int(sum / count); // todo float
-}
-
-getType(var)
-{
-
-}
-
-orderBy(array)
-{
-
-}
-
-sort(array)
-{
-
-}
-
-sortType(array, type)
-{
-
-}
-
-cast(array, type)
-{
-
-}
-
-first(array, predicate)
-{
-    ret = undefined;
-    for (i = 0; i < array.size; i++)
-    {
-        if ([[predicate]](array[i]))
-            return array[i];
-    }
-    return ret;
-}
-
-last(array, predicate)
-{
-    ret = undefined;
-    for (i = 0; i < array.size; i++)
-    {
-        if ([[predicate]](array[i]))
-            ret = array[i];
-    }
-    return ret;
-}
-
 max(array)
 {
     value = array[0];
@@ -211,14 +81,4 @@ min(array)
             value = array[i];
     }
     return value;
-}
-
-any(array, predicate)
-{
-    for (i = 0; i < array.size; i++)
-    {
-        if ([[predicate]](array[i]))
-            return true;
-    }
-    return false;
 }
--- gsclinq.c
+++ gsclinq.c
@@ -1,11 +1,11 @@
-#include "cgsc.h"
+#include "dependencies/cgsc.h"
 #include "gsclinq.h"
 
 void LINQ_All()
 {
 	if (Plugin_Scr_GetNumParam() != 3)
     {
-		Plugin_Scr_Error("Usage: all(<array>, <array size>, <::function>)");
+		Plugin_Scr_Error("Usage: all(<array>, <array size>, <::predicate>)");
 		return;
     }
 	VariableValue **array = Plugin_Scr_GetArray(0);
@@ -32,11 +32,61 @@ void LINQ_All()
 	Plugin_Scr_AddBool(result);
 }
 
+void LINQ_Reverse()
+{
+	if (Plugin_Scr_GetNumParam() != 2)
+    {
+		Plugin_Scr_Error("Usage: reverse(<array>, <array size>)");
+		return;
+    }
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+
+	Plugin_Scr_MakeArray();
+	for (int i = length - 1; i > -1; i--)
+	{
+		Plugin_Scr_AddVariable(array[i]);
+		Plugin_Scr_AddArray();
+	}
+	Plugin_Scr_FreeArray(array, length);
+}
+
+void LINQ_Any()
+{
+	if (Plugin_Scr_GetNumParam() != 3)
+    {
+		Plugin_Scr_Error("Usage: any(<array>, <array size>, <::predicate>)");
+		return;
+    }
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+	const uint32_t threadId = Plugin_Scr_GetFunc(2);
+	qboolean result = qfalse;
+
+	for (int i = 0; i < length; i++)
+	{
+		// Call predicate(item)
+		Plugin_Scr_AddVariable(array[i]);
+		const short tid = Plugin_Scr_ExecThread(threadId, 1);
+		const register int *gscPredicate asm("edx");
+
+		if (*gscPredicate)
+		{
+			result = qtrue;
+			Plugin_Scr_FreeThread(tid);
+			break;
+		}
+		Plugin_Scr_FreeThread(tid);
+	}
+	Plugin_Scr_FreeArray(array, length);
+	Plugin_Scr_AddBool(result);
+}
+
 void LINQ_Where()
 {
 	if (Plugin_Scr_GetNumParam() != 3)
 	{
-		Plugin_Scr_Error("Usage: where(<array>, <array_size>, <::function>)");
+		Plugin_Scr_Error("Usage: where(<array>, <array_size>, <::predicate>)");
 		return;
 	}
 	VariableValue **array = Plugin_Scr_GetArray(0);
@@ -61,7 +111,7 @@ void LINQ_Where()
 	Plugin_Scr_FreeArray(array, length);
 }
 
-void comPrintf()
+void ComPrintf()
 {
 	if (Plugin_Scr_GetNumParam() != 1)
     {
@@ -71,11 +121,318 @@ void comPrintf()
 	Plugin_Printf(Plugin_Scr_GetString(0));
 }
 
+
+void LINQ_Min()
+{
+	if (Plugin_Scr_GetNumParam() != 2)
+	{
+		Plugin_Scr_Error("Usage: min(<array>, <array_size>)");
+		return;
+	}
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+
+	for (int i = 0; i < length; i++)
+	{
+		// TODO type sort in their own function
+	}
+	Plugin_Scr_FreeArray(array, length);
+}
+
+void LINQ_Max()
+{
+	if (Plugin_Scr_GetNumParam() != 2)
+	{
+		Plugin_Scr_Error("Usage: max(<array>, <array_size>)");
+		return;
+	}
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+
+	for (int i = 0; i < length; i++)
+	{
+		// TODO type sort in their own function
+	}
+	Plugin_Scr_FreeArray(array, length);
+}
+
+void LINQ_Last()
+{
+	if (Plugin_Scr_GetNumParam() != 3)
+    {
+		Plugin_Scr_Error("Usage: last(<array>, <array size>, <::predicate>)");
+		return;
+    }
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+	const uint32_t threadId = Plugin_Scr_GetFunc(2);
+	VariableValue *last = NULL;
+
+	for (int i = 0; i < length; i++)
+	{
+		// Call predicate(item)
+		Plugin_Scr_AddVariable(array[i]);
+		const short tid = Plugin_Scr_ExecThread(threadId, 1);
+		const register int *gscPredicate asm("edx");
+
+		if (*gscPredicate)
+			last = array[i];
+		Plugin_Scr_FreeThread(tid);
+	}
+	Plugin_Scr_AddVariable(last);
+	Plugin_Scr_FreeArray(array, length);
+}
+
+void LINQ_First()
+{
+	if (Plugin_Scr_GetNumParam() != 3)
+    {
+		Plugin_Scr_Error("Usage: first(<array>, <array size>, <::predicate>)");
+		return;
+    }
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+	const uint32_t threadId = Plugin_Scr_GetFunc(2);
+
+	for (int i = 0; i < length; i++)
+	{
+		// Call predicate(item)
+		Plugin_Scr_AddVariable(array[i]);
+		const short tid = Plugin_Scr_ExecThread(threadId, 1);
+		const register int *gscPredicate asm("edx");
+
+		if (*gscPredicate)
+		{
+			Plugin_Scr_AddVariable(array[i]);
+			Plugin_Scr_FreeThread(tid);
+			break;
+		}
+		Plugin_Scr_FreeThread(tid);
+	}
+	Plugin_Scr_FreeArray(array, length);
+}
+
+void LINQ_Cast()
+{
+	if (Plugin_Scr_GetNumParam() != 3)
+	{
+		Plugin_Scr_Error("Usage: cast(<array>, <array_size>, <type>)");
+		return;
+	}
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+	const char *typename = Plugin_Scr_GetString(2);
+
+	for (int i = 0; i < length; i++)
+	{
+		// TODO type sort in their own function
+	}
+	Plugin_Scr_FreeArray(array, length);
+}
+
+void LINQ_Sort()
+{
+	if (Plugin_Scr_GetNumParam() != 2)
+	{
+		Plugin_Scr_Error("Usage: sort(<array>, <array_size>)");
+		return;
+	}
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+
+	for (int i = 0; i < length; i++)
+	{
+		// TODO type sort in their own function
+	}
+	Plugin_Scr_FreeArray(array, length);
+}
+
+void LINQ_OrderBy()
+{
+	if (Plugin_Scr_GetNumParam() != 3)
+	{
+		Plugin_Scr_Error("Usage: orderby(<array>, <array_size>, <::predicate>)");
+		return;
+	}
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+	const uint32_t threadId = Plugin_Scr_GetFunc(2);
+
+	Plugin_Scr_MakeArray();
+	for (int i = 0; i < length; i++)
+	{
+		// TODO type sort in their own function
+	}
+	Plugin_Scr_FreeArray(array, length);
+}
+
+void LINQ_Average()
+{
+	if (Plugin_Scr_GetNumParam() != 2)
+    {
+		Plugin_Scr_Error("Usage: average(<array>, <array size>)");
+		return;
+    }
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+	float sum = 0, count = 0;
+
+	for (int i = 0; i < length; i++)
+	{
+		if (array[i]->type == VAR_FLOAT)
+			sum += array[i]->u.floatValue;
+		else
+			sum += array[i]->u.intValue;
+		count++;
+	}
+
+	Plugin_Scr_FreeArray(array, length);
+	if (length < 1 || !count)
+		Plugin_Scr_AddBool(qfalse);
+	else
+		Plugin_Scr_AddFloat(sum / count);
+}
+
+void LINQ_Count()
+{
+	if (Plugin_Scr_GetNumParam() != 3)
+    {
+		Plugin_Scr_Error("Usage: count(<array>, <array size>, <::predicate>)");
+		return;
+    }
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+	const uint32_t threadId = Plugin_Scr_GetFunc(2);
+	int result = 0;
+
+	for (int i = 0; i < length; i++)
+	{
+		// Call predicate(item)
+		Plugin_Scr_AddVariable(array[i]);
+		const short tid = Plugin_Scr_ExecThread(threadId, 1);
+		const register int *gscPredicate asm("edx");
+
+		if (*gscPredicate)
+			result++;
+
+		Plugin_Scr_FreeThread(tid);
+	}
+	Plugin_Scr_FreeArray(array, length);
+	Plugin_Scr_AddInt(result);
+}
+
+void LINQ_Sum()
+{
+	if (Plugin_Scr_GetNumParam() != 2)
+	{
+		Plugin_Scr_Error("Usage: sum(<array>, <array_size>)");
+		return;
+	}
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+
+	for (int i = 0; i < length; i++)
+	{
+		// TODO type sort in their own function
+	}
+	Plugin_Scr_FreeArray(array, length);
+}
+
+void LINQ_Select()
+{
+	if (Plugin_Scr_GetNumParam() != 3)
+	{
+		Plugin_Scr_Error("Usage: select(<array>, <array_size>, <::delegate>)");
+		return;
+	}
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+	const uint32_t threadId = Plugin_Scr_GetFunc(2);
+
+	Plugin_Scr_MakeArray();
+	for (int i = 0; i < length; i++)
+	{
+		// Call predicate(item)
+		Plugin_Scr_AddVariable(array[i]);
+		const short tid = Plugin_Scr_ExecThread(threadId, 1);
+		const register int *selectResult asm("edx");
+
+		//Plugin_Scr_AddVariable(*selectResult); // TODO get returned variableValue from execthread
+		Plugin_Scr_AddArray();
+		Plugin_Scr_FreeThread(tid);
+	}
+	Plugin_Scr_FreeArray(array, length);
+}
+
+void LINQ_Range()
+{
+	if (Plugin_Scr_GetNumParam() != 4)
+    {
+		Plugin_Scr_Error("Usage: range(<array>, <array size>, <min>, <max>)");
+		return;
+    }
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+	const uint32_t min = Plugin_Scr_GetInt(2);
+	const uint32_t max = Plugin_Scr_GetInt(3);
+
+	if (min >= max || max < min)
+	{
+		Plugin_Scr_Error("range() - wrong min/max value");
+		return;
+    }
+
+	Plugin_Scr_MakeArray();
+	for (int i = min; i < max; i++)
+	{
+		Plugin_Scr_AddVariable(array[i]);
+		Plugin_Scr_AddArray();
+	}
+	Plugin_Scr_FreeArray(array, length);
+}
+
+void LINQ_Repeat()
+{
+	if (Plugin_Scr_GetNumParam() != 3)
+    {
+		Plugin_Scr_Error("Usage: repeat(<array>, <array size>, <count>)");
+		return;
+    }
+	VariableValue **array = Plugin_Scr_GetArray(0);
+	const uint32_t length = Plugin_Scr_GetInt(1);
+	const uint32_t count = Plugin_Scr_GetInt(2);
+
+	Plugin_Scr_MakeArray();
+	for (int r = 0; r < count; r++)
+	{
+		for (int i = 0; i < length; i++)
+		{
+			Plugin_Scr_AddVariable(array[i]);
+			Plugin_Scr_AddArray();
+		}
+	}
+	Plugin_Scr_FreeArray(array, length);
+}
+
 PCL int OnInit()
 {
-	Plugin_ScrAddFunction("comPrintf", 		&comPrintf);
+	Plugin_ScrAddFunction("comPrintf", 		&ComPrintf);
 	Plugin_ScrAddFunction("all", 			&LINQ_All);
 	Plugin_ScrAddFunction("where", 			&LINQ_Where);
+	Plugin_ScrAddFunction("any", 			&LINQ_Any);
+	Plugin_ScrAddFunction("min", 			&LINQ_Min);
+	Plugin_ScrAddFunction("max", 			&LINQ_Max);
+	Plugin_ScrAddFunction("last",			&LINQ_Last);
+	Plugin_ScrAddFunction("first", 			&LINQ_First);
+	Plugin_ScrAddFunction("cast", 			&LINQ_Cast);
+	Plugin_ScrAddFunction("orderby", 		&LINQ_OrderBy);
+	Plugin_ScrAddFunction("average", 		&LINQ_Average);
+	Plugin_ScrAddFunction("count", 			&LINQ_Count);
+	Plugin_ScrAddFunction("sum", 			&LINQ_Sum);
+	Plugin_ScrAddFunction("select", 		&LINQ_Select);
+	Plugin_ScrAddFunction("range", 			&LINQ_Range);
+	Plugin_ScrAddFunction("repeat", 		&LINQ_Repeat);
+	Plugin_ScrAddFunction("reverse", 		&LINQ_Reverse);	
 
 	return 0;
 }
