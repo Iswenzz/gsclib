64b7b24ca014730eb341e88870e8b6272db119c5
Iswenzz <alexisnardiello@gmail.com>
2022-06-30 16:51:23 +0200

    Tests: Fixed vsnprintf test

--- src/utils/__test__/vsnprintf.test.c
+++ src/utils/__test__/vsnprintf.test.c
@@ -10,7 +10,9 @@ TEST test_vsnprintf()
 	const int argCount = 2;
 
 	VariableValueArray args = Plugin_Scr_CreateArray(argCount);
+	args.items[0].type = VAR_INTEGER;
 	args.items[0].u.intValue = 1;
+	args.items[1].type = VAR_INTEGER;
 	args.items[1].u.intValue = 2;
 
 	Scr_vsnprintf(buffer, sizeof(buffer), format, args);
