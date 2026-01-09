9475decba51a6d852433b557e26eb80381a66969
Alexis Nardiello <alexisnardiello@gmail.com>
2026-01-09 18:46:19 +0100

    Small changes

--- cmake/ClangTools.cmake
+++ cmake/ClangTools.cmake
@@ -1,11 +1,11 @@
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 add_custom_target(clang-tidy
-	COMMAND find . -type f \( -name '*.c' -o -name '*.h' \) -not -path './build/*' -exec clang-tidy --config-file=.clang-tidy {} +
+	COMMAND find . -type f \( -name '*.cpp' -o -name '*.hpp' \) -not -path './build/*' -exec clang-tidy --config-file=.clang-tidy {} +
 	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 
 add_custom_target(clang-format
-	COMMAND find . -type f \( -name '*.c' -o -name '*.h' \) -not -path './build/*' -exec clang-format -style=file -i --verbose {} +
+	COMMAND find . -type f \( -name '*.cpp' -o -name '*.hpp' \) -not -path './build/*' -exec clang-format -style=file -i --verbose {} +
 	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 
 set_target_properties(clang-tidy PROPERTIES FOLDER Clang)
