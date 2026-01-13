22fd4cedbb36a1764e3abff99e1fe3abeb0d09de
Alexis Nardiello <alexisnardiello@gmail.com>
2026-01-13 13:35:02 +0100

    Removed cgsc

--- cgsc/sys/compatibility.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "CGSC/cgsc.h"
-
-EXPORT(float, Sys_GetCommonVersion());
-EXPORT(float, CGSC_Version());
-EXPORT(qboolean, CGSC_Unsupported(qboolean versionCondition));
-EXPORT(qboolean, CGSC_UnsupportedMessage(qboolean versionCondition, char *format, ...));
-EXPORT(qboolean, CGSC_SupportIndexedString());
--- cgsc/tests/_suite.gsc
+++
@@ -1,8 +0,0 @@
-#include sr\libs\gscunit\_main;
-
-cgsc()
-{
-	suite("CGSC");
-
-	sr\libs\cgsc\__test__\_test_variables::main();
-}
